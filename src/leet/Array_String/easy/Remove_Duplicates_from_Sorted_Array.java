e98a2b869bc2d1d13d6862f5d15f14d765db527d
Brunofell <brunofelimarti@gmail.com>
2025-04-14 15:07:25 -0300

    sla

--- src/leet/Array_String/easy/Remove_Duplicates_from_Sorted_Array.java
+++ src/leet/Array_String/easy/Remove_Duplicates_from_Sorted_Array.java
@@ -1,29 +1,23 @@
 package leet.Array_String.easy;
 
+import java.util.Arrays;
+
 public class Remove_Duplicates_from_Sorted_Array {
 
     public static void main(String[] args) {
-        int[] nums = {1,1,2};
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         removeDuplicates(nums);
     }
 
-
     public static int removeDuplicates(int[] nums) {
-        int k = 0;
-        int j = 0;
-        for(int i = 0; i < nums.length; i++){
-            j = nums[i];
-            if(nums[i] == );
+        int k = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[k] = nums[i];
+                k++;
+            }
         }
-
+        return k;
     }
-}
 
-/*
-*
-* pegar array
-* pegar item da vez, e abrir outro for dentro, rodar e ver o que tem de repetido
-* se for repetido
-*
-*
-* */
+}
